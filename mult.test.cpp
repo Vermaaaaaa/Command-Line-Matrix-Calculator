2853489cfc5becb69741c9ef729d82b82f5217e6
willllllyly <el22wb@leeds.ac.uk>
2023-12-05 22:25:54 +0000

    Trying to implement the mult test function

--- mult.test.cpp
+++ mult.test.cpp
@@ -1,17 +1,55 @@
+#include <cassert>
 #include "multiply.h"
 #include "matrix.h"
 #include <iostream>
 
-void ans = 0.0;
+int main(){
+    Matrix mat1(2,2), mat2(2,2), mat3(2,2), mat4(2,2);
 
-int main() {
+    mat1.set_index(0,0,2);
+    mat1.set_index(0,1,4);
+    mat1.set_index(1,0,6);
+    mat1.set_index(1,1,8);
 
+    
+    mat2.set_index(0,0,3);
+    mat2.set_index(0,1,4);
+    mat2.set_index(1,0,6);
+    mat2.set_index(1,1,5);
+
+    mat3 = multiply2(mat1,mat2);
+    mat4 = multiply2(mat1,mat2);
+
+    assert(mat3.get_index(0, 0) == 5);
+    assert(mat3.get_index(0, 1) == 8);
+    assert(mat3.get_index(1, 0) == 12);
+    assert(mat3.get_index(1, 1) == 13);
+    
+    // If any of these assertions fail, the program will terminate with an error.
+
+    // Additional checks, you might want to verify the size of the matrices.
+    assert(mat3.get_row() == 2);
+    assert(mat3.get_col() == 2);
+
+    assert(mat4.get_index(0, 0) == -1);
+    assert(mat4.get_index(0, 1) == 0);
+    assert(mat4.get_index(1, 0) == 0);
+    assert(mat4.get_index(1, 1) == 3);
+
+    // If any of these assertions fail, the program will terminate with an error.
+
+    // Additional checks, you might want to verify the size of the matrices.
+    assert(mat4.get_row() == 2);
+    assert(mat4.get_col() == 2);
+
+    std::cout << "All tests passed" << std::endl;
+
+    return 0;
 }
 
 
 
-
-bool test_mult(const Matrix &mat1, const Matrix &mat2, const Matrix expected){
+/*bool test_mult(const Matrix &mat1, const Matrix &mat2, const Matrix expected){
     std::cout << "mult(" << mat1 << "," << mat2 << ") : ";
     ans = multiply(std::shared_ptr<std::unordered_map<std::string, Matrix>> &map);
      if (ans == expected) {
@@ -34,4 +72,4 @@ bool run_test_mult() {
     if (test_mult()) passed ++;
     std::cout << "Multiples passed " << passed << " tests." << std::endl;
     return passed;
-}
+}*/
--- multiply.cpp
+++ multiply.cpp
@@ -9,6 +9,8 @@
     return false;
 
 }*/
+bool mat1;
+bool mat2;
 
 void multiply(std::shared_ptr<std::unordered_map<std::string, Matrix>> &map){ //Shared map for the matrices that have been inputted
     Matrix mat1, mat2;
@@ -46,4 +48,34 @@ void multiply(std::shared_ptr<std::unordered_map<std::string, Matrix>> &map){ //
         if(choice == "Y"){matrix_save(map, mult_mat); save_flag = true;}
     }
     while(!save_flag);
+}
+
+//Seperate multiply function to accomodate for the test function
+bool multiply2(mat1,mat2){ //Shared map for the matrices that have been inputted
+    Matrix mat1, mat2;
+    bool found_flag1 = false;
+    bool found_flag2 = false;
+    do{
+        mat1 = select(map, found_flag1);
+        mat2 = select(map, found_flag2);
+    }
+    while(!found_flag1 || !found_flag2); // Carry on only if both matrices are found
+
+    if (mat1.get_col() != mat2.get_row()){ // Conformable check
+        std::cout << "Matrices are not conformable and cannot be multiplied" << std::endl;
+        return;
+    }
+
+    Matrix mult_mat(mat1.get_row(), mat2.get_col()); // The size of answer will be the amount of rows from mat1 and columns from mat2
+    for (int i = 0; i < mat1.get_row(); ++i) {
+        for (int j = 0; j < mat2.get_col(); ++j) {
+            double mult_ans = 0.0;
+            for (int k = 0; k < mat1.get_col(); ++k) {
+                mult_ans += mat1.get_index(i, k) * mat2.get_index(k, j); // Matrix multiplication iteration sequence
+            }
+            mult_mat.set_index(i, j, mult_ans); // Set new index values to answer
+        }        
+    }
+    mult_mat.display(); // Call display function to show answer
+    
 }
