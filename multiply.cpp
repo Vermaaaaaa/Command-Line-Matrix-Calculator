ae6072d754c0a8b2ca3555a7fb90b215a74db01f
willllllyly <el22wb@leeds.ac.uk>
2023-12-12 14:31:25 +0000

    Update multiply.cpp

--- multiply.cpp
+++ multiply.cpp
@@ -2,6 +2,11 @@
 #include "matrix.h"
 #include <iostream>
 
+int main() {
+    bool mat1;
+    bool mat2;
+}
+
 /*bool is_c(const Matrix &mat1, const Matrix &mat2 ){
     if (mat1.get_col() == mat2.get_row()){
         return true;
@@ -9,8 +14,8 @@
     return false;
 
 }*/
-bool mat1;
-bool mat2;
+//bool mat1;
+//bool mat2;
 
 void multiply(std::shared_ptr<std::unordered_map<std::string, Matrix>> &map){ //Shared map for the matrices that have been inputted
     Matrix mat1, mat2;
@@ -20,7 +25,7 @@ void multiply(std::shared_ptr<std::unordered_map<std::string, Matrix>> &map){ //
         mat1 = select(map, found_flag1);
         mat2 = select(map, found_flag2);
     }
-    while(!found_flag1 || !found_flag2); // Carry on only if both matrices are found
+    while(!found_flag1 || !found_flag2); // Carry on, only if both matrices are found
 
     if (mat1.get_col() != mat2.get_row()){ // Conformable check
         std::cout << "Matrices are not conformable and cannot be multiplied" << std::endl;
@@ -28,10 +33,10 @@ void multiply(std::shared_ptr<std::unordered_map<std::string, Matrix>> &map){ //
     }
 
     Matrix mult_mat(mat1.get_row(), mat2.get_col()); // The size of answer will be the amount of rows from mat1 and columns from mat2
-    for (int i = 0; i < mat1.get_row(); ++i) {
+    for (int i = 0; i < mat1.get_row(); ++i) { //Iterate through the rows of mat 1 then cols of mat2, to carry out the multiplication
         for (int j = 0; j < mat2.get_col(); ++j) {
-            double mult_ans = 0.0;
-            for (int k = 0; k < mat1.get_col(); ++k) {
+            double mult_ans = 0.0; // Initialise empty answer variable
+            for (int k = 0; k < mat1.get_col(); ++k) { // Only need to carry out the multiplciation for as many cols in mat1
                 mult_ans += mat1.get_index(i, k) * mat2.get_index(k, j); // Matrix multiplication iteration sequence
             }
             mult_mat.set_index(i, j, mult_ans); // Set new index values to answer
@@ -45,21 +50,14 @@ void multiply(std::shared_ptr<std::unordered_map<std::string, Matrix>> &map){ //
         std::cout << "\nWould you like to save this matrix (Y/N)";
         std::cin >> choice;
         if(choice == "N"){save_flag = true;}
-        if(choice == "Y"){matrix_save(map, mult_mat); save_flag = true;}
+        if(choice == "Y"){matrix_save(map, mult_mat); save_flag = true;} // Breaks loop
     }
-    while(!save_flag);
+    while(!save_flag); // Check for loop break
 }
 
 //Seperate multiply function to accomodate for the test function
 bool multiply2(mat1,mat2){ //Shared map for the matrices that have been inputted
     Matrix mat1, mat2;
-    bool found_flag1 = false;
-    bool found_flag2 = false;
-    do{
-        mat1 = select(map, found_flag1);
-        mat2 = select(map, found_flag2);
-    }
-    while(!found_flag1 || !found_flag2); // Carry on only if both matrices are found
 
     if (mat1.get_col() != mat2.get_row()){ // Conformable check
         std::cout << "Matrices are not conformable and cannot be multiplied" << std::endl;
